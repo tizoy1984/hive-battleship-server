513ac7ff03b407eb77a6fec2525879958a69d118
tizoy1984 <marius.werk@gmail.com>
2026-02-25 23:30:27 +0100

    Update server.js

--- server.js
+++ server.js
@@ -107,7 +107,7 @@ io.on('connection', (socket) => {
         if (isHit) {
             game.hits[socket.id] += 1; 
             
-            if (game.hits[socket.id] >= 17) {
+            if (game.hits[socket.id] >= 1) {
                 console.log(`🏆 GAME OVER! ${attacker.username} won! Initiating payout...`);
                 
                 // Trigger the automatic blockchain payout!
@@ -137,3 +137,4 @@ const PORT = 3000;
 server.listen(PORT, () => {
     console.log(`🚀 Battleship Server draait op http://localhost:${PORT}`);
 });
+
